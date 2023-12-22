55d496b7879f18b809271b84488824c1ccc68cec
Laura Kane <148717413+kauralane@users.noreply.github.com>
2023-12-22 18:40:33 +0000

    added starter code and drafted questions objects.

---
+++ assets/javascript/questions.js
@@ -0,0 +1,37 @@
+// Questions 
+
+const questionOne = {
+    question: 'What blah blah blah', // string
+    option1: 'answer one', // string
+    option2: 'answer two', // string
+    answer: option1
+}
+
+const questionTwo = {
+    question: 'What blah blah blah', // string
+    option1: 'answer one', // string
+    option2: 'answer two', // string
+    answer: option2
+}
+
+const questionThree = {
+    question: 'What blah blah blah', // string
+    option1: 'answer one', // string
+    option2: 'answer two', // string
+    answer: option1
+}
+
+const questionFour = {
+    question: 'What blah blah blah', // string
+    option1: 'answer one', // string
+    option2: 'answer two', // string
+    answer: option2
+}
+
+const questionFive = {
+    question: 'What blah blah blah', // string
+    option1: 'answer one', // string
+    option2: 'answer two', // string
+    answer: option2
+}
+
